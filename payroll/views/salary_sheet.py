bd29998b837318bab8b9f585cd92f3e758a17c2d
JABIR HOSSAIN <68606785+Jabirhossan@users.noreply.github.com>
2026-09-25 21:26:44 +0600

    Fix salary sheet import syntax

--- payroll/views/salary_sheet.py
+++ payroll/views/salary_sheet.py
@@ -6,9 +6,9 @@ from calendar import monthrange
 from datetime import date
 
 import pandas as pd
-from django.db.models import Q\nfrom django.http import HttpResponse
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_GET
 
